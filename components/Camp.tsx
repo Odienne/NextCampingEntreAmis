fed75d5e04bd9054afeee5e42750b941a446a91b
Adam Odienne <odienne.adam@gmail.com>
2023-11-15 11:09:44 +0100

    finishing project and adding config for build

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,11 +1,79 @@
 import Link from "next/link";
 import Image from "next/image";
-import {NAV_LINKS} from "@/constants";
+import {NAV_LINKS, PEOPLE_URL} from "@/constants";
 import Button from "@/components/Button";
+
+interface CampProps {
+    backgroundImage: string;
+    title: string;
+    subtitle: string;
+    peopleJoined: string;
+}
+
+const CampSite = ({backgroundImage, title, subtitle, peopleJoined}: CampProps) => {
+    return (
+        <div
+            className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}>
+            <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
+                <div className="flexCenter gap-4">
+                    <div className="rounded-full bg-green-50 p-4">
+                        <Image src="/folded-map.svg" alt="map" width={38} height={38}/>
+                    </div>
+                    <div className="flex flex-col gap-1">
+                        <h4 className="bold-18 text-white">{title}</h4>
+                        <p className="regular-16 text-white">{subtitle}</p>
+                    </div>
+                </div>
+
+                <div className="flexCenter gap-6">
+                    <span className="flex -space-x-4">
+                        {PEOPLE_URL.map((url) => (
+                            <Image className="inline-block h-10 w-10 rounded-full" src={url} key={url} alt="person"
+                                   width={52} height={52}/>
+                        ))}
+                    </span>
+                    <p className="bold-16 md:bold-20 text-white">
+
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export default function Camp() {
     return (
-        <>
-        </>
+        <section className="relative 2xl:max-container flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
+            <div
+                className="hide-scrollbar flex h-[340px] w-full
+                 items-start justify-start gap-8
+                 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+
+                <CampSite backgroundImage="bg-bg-img-1"
+                          title="Putuk Truno Camping"
+                          subtitle="Prigen, Pasuruan"
+                          peopleJoined="50+ ont rejoint"/>
+
+                <CampSite backgroundImage="bg-bg-img-1"
+                          title="Vue sur les montagnes"
+                          subtitle="Dans la nature sauvage"
+                          peopleJoined="10+ ont rejoint"/>
+
+            </div>
+
+            <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
+                <div
+                    className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px]
+                     xl-rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
+                    <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white"><strong>Perdu
+                        ?</strong></h2>
+                    <p className="regular-14 xl:regular-16 mt-5 text-white">
+                        A cause de l'anxiété lié à la découverte d'un lieu inconnu, il est possible de se perdre. C'est
+                        pourquoi nous sommes là pour vous.
+                    </p>
+                    <Image src="/quote.svg" alt="camp-quote" width={186} height={219} className="camp-quote"/>
+                </div>
+            </div>
+        </section>
     )
 }
--- components/Guide.tsx
+++ components/Guide.tsx
@@ -5,7 +5,47 @@ import Button from "@/components/Button";
 
 export default function Guide() {
     return (
-        <>
-        </>
+        <section className="flexCenter flex-col">
+            <div className="padding-container max-container w-full pb-24">
+                <Image src="/camp.svg" alt="camp" width={50} height={50}/>
+                <p className="uppercase regular-18 -mt-1 mb-3 text-green-50">
+                    Nous sommes là pour vous
+                </p>
+                <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
+                    <h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">Votre guide pour un chemin facile</h2>
+                    <p className="regular-16 text-gray-30 xl:max-w-[520px">
+                        Avec notre application Hilink, vous n'allez plus jamais vous perdre. Nous prenons en charge les
+                        cartes hors-ligne pour s'assurer que même sans connexion internet, nous pourrons vous aider.
+                        Invitez vos amis et vos proches pour vous amuser dans la nature sauvage.
+                    </p>
+                </div>
+
+            </div>
+
+            <div className="flexCenter max-container relative w-full">
+                <Image className="w-full object-cover object-center 2xl:rounded-5xl" src="/boat.png" alt="boat"
+                       width={1440} height={580}/>
+                <div
+                    className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
+                    <Image className="h-full w-auto" src="/meter.svg" alt="meter" width={16} height={158}/>
+                    <div className="flexBetween flex-col">
+                        <div className="flex w-full flex-col">
+                            <div className="flexBetween w-full">
+                                <p className="regular-16 text-gray-20">Destination</p>
+                                <p className="bold-16 text-green-50">48 min</p>
+                            </div>
+                            <p className="bold-20 mt-2">Aguas calientes</p>
+                        </div>
+
+                        <div className="flex w-full flex-col">
+                            <p className="regular-16 text-gray-20">Départ</p>
+                            <h4 className="bold-20 mt-2 whitespace-nowrap">Wonorejo Pasuruan</h4>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </section>
     )
 }
